b3a06b80d813eed71fef2eb143dfafb4655f349c
hpnightowl <iamhp2k@gmail.com>
2019-08-22 08:55:37 +0530

    Added Codes

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+#x=input("Enter Radius Of Circle") #general case
+x=7
+area=float(3.14*x*x)
+print("Area Of Circle:=",area)
---
+++ gst.py
@@ -0,0 +1,5 @@
+x=float(input("enter a cost"))
+gst=float(x*0.18)
+netcost=float(x-gst)
+print(netcost)
+
