08c66ded5db7a1c9051d3a8dd3befced922ff6e9
HARSH <iamhp2k@gmail.com>
2019-08-31 16:03:00 +0530

    Added Codes

---
+++ linemidpoint.py
@@ -0,0 +1,11 @@
+#x=float(input("Enter The initial x-axis Point"))
+#y=float(input("Enter The initial y-axis Point"))
+#x1=float(input("Enter The final x-axis Point"))
+#y1=float(input("Enter The final y-axis Point"))
+x1=2
+x=4
+y=2
+y1=4
+mid_pointx=int((x+x1)/2)
+mid_pointy=int((y+y1)/2)
+print("Mid Point is :",mid_pointx,mid_pointy)
